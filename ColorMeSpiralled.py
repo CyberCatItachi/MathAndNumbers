23940cab62c933f265348cf994388169a2faa6e3
Viktoria Mashtakova <artemenko.viktori@yandex.ru>
2023-11-06 15:54:40 +0300

    hw done

--- ColorMeSpiralled.py
+++ ColorMeSpiralled.py
@@ -1,12 +1,17 @@
 import turtle
 t = turtle.Pen()
 turtle.bgcolor("black")
+#
 colors = ["red", "yellow", "blue" , "green", "orange", "purple", "white", "gray"]
+#
+your_name = turtle.textinput("Введите свое имя", "Как тебя зовут?")
+#
 sides = int(turtle.numinput("Сколько сторон", "Сколько сторон вы хотите?(1-8)", 4, 1, 8))
 for x in range (100):
+    #
     t.pencolor(colors[x%sides])
-    #t.circle(x * 3 / sides + x)
+    t.penup()
     t.forward(x * 3 / sides + x)
+    t.pendown()
+    t.write(your_name, font = ("Arial", int((x + sides)/sides), "bold"))
     t.left(360/sides + 1)
-    #t.left(360/sides)
-    t.width(x * sides / 200)
